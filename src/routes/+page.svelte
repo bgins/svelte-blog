82279f778a6cd380afa2f8b121cf73efad7d3a47
Brian Ginsburg <gins@brianginsburg.com>
2024-05-14 11:46:28 -0700

    Remove unneeded data-sveltekit-preload-data
    Applied by default to body.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,7 +38,7 @@
   <div id="content">
     {#await promise then posts}
       {#each posts as post}
-        <a href={post.slug} data-sveltekit-preload-data>
+        <a href={post.slug}>
           <div class="entry">
             <h2>{post.title}</h2>
             <div class="published">{formatDate(post.published)}</div>
