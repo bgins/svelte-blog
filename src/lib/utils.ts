efd7d7a0bb3316f9e4422a8227afcd9d28410c70
Brian Ginsburg <gins@brianginsburg.com>
2024-05-15 14:36:13 -0700

    Fix prerendering
    Internationalization of dates only happens in the browser.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,17 @@
-export function formatDate(dateString: string): string {
-  const timestamp: number = Date.parse(`${dateString}`)
-  const date: Date = new Date(+timestamp)
-  const lang: string = navigator.language
-  const formatOptions: Intl.DateTimeFormatOptions = {
-    dateStyle: 'long'
-  }
+import { browser } from '$app/environment'
 
-  const formattedDate: string = new Intl.DateTimeFormat(lang, formatOptions).format(date)
-  return formattedDate
+export function formatDate(dateString: string): string {
+  if (browser) {
+    const timestamp: number = Date.parse(`${dateString}`)
+    const date: Date = new Date(+timestamp)
+    const lang: string = navigator.language
+    const formatOptions: Intl.DateTimeFormatOptions = {
+      dateStyle: 'long'
+    }
+
+    const formattedDate: string = new Intl.DateTimeFormat(lang, formatOptions).format(date)
+    return formattedDate
+  } else {
+    return dateString
+  }
 }
